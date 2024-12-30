65e4c4b5284d938d3e48c2b823192f7b4c2219e2
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-12-30 22:25:35 +0530

    day-6 of express

--- express-js/day-1/index.js
+++ express-js/day-1/index.js
@@ -1,136 +1,122 @@
 import express from "express";
-
+import userData from "./data/data.js";
 const app = express();
-// middleware for parsing json data
 app.use(express.json());
-const PORT = process.env.PORT || 8000;
 
-const userData = [
-  { id: 1, name: "John Doe", displayname: "John D" },
-  { id: 2, name: "Jane Doe", displayname: "Jane D" },
-  { id: 3, name: "John Smith", displayname: "John S" },
-  { id: 4, name: "Jane Smith", displayname: "Jane S" },
-  { id: 5, name: "John Wick", displayname: "John W" },
-  { id: 6, name: "Jane Wick", displayname: "Jane W" },
-  { id: 7, name: "John Cena", displayname: "John C" },
-  { id: 8, name: "Jane Cena", displayname: "Jane C" },
-];
+const PORT = 8080;
 
-// * GET REQUESTS
+// *1. GET Request ( it is for fetching data from server)
+
 app.get("/", (req, res) => {
-  res.status(200).send({ msg: "Hello World" });
+  res.status(200).send("Hello! World");
 });
 
-// Industry standards
+// Industry Standards
 app.get("/api/v1/users", (req, res) => {
-  console.log(req.query);
+  const { name } = req.query;
+
+  if (name) {
+    const user = userData.filter((user) => {
+      return user.name === name;
+    });
+    res.status(200).send(user);
+  }
+
+  // query params
   res.status(200).send(userData);
 });
 
-// routes parameters
+// router params
 app.get("/api/v1/users/:id", (req, res) => {
-  const id = req.params.id;
+  const { id } = req.params;
   const parsedId = parseInt(id);
-  if (isNaN(parsedId)) {
-    res.status(400).send({ msg: "Invalid ID" });
-  }
 
   const user = userData.find((user) => user.id === parsedId);
+
   res.status(200).send(user);
 });
 
-app.get("/api/v1/producsts", (req, res) => {
-  res.status(200).send([
-    { id: 1, name: "Iphone 13" },
-    { id: 2, name: "Samsung S21" },
-    { id: 3, name: "One Plus 9" },
-    { id: 4, name: "Google Pixel 6" },
-  ]);
-});
-
-// * POST REQUESTS
+// *2. POST Request ( it is for sending data to server)
 
 app.post("/api/v1/users", (req, res) => {
-  console.log(req.body);
-  const newUser = { id: userData.length + 1, ...req.body };
+  const { name, displayname } = req.body;
+
+  const newUser = {
+    id: userData.length + 1,
+    name,
+    displayname,
+  };
+
   userData.push(newUser);
 
-  res.status(201).send(newUser);
+  res.status(201).send({
+    message: "User Created",
+    data: newUser,
+  });
 });
 
-// * PUT REQUESTS ( update all fields)
+// *3. PUT Request ( UPDATE ALL FIELDS);
+
 app.put("/api/v1/users/:id", (req, res) => {
   const {
     body,
     params: { id },
+
+    
   } = req;
 
   const parsedId = parseInt(id);
+  const userIndex = userData.findIndex((user)=>user.id === parsedId);
 
-  if (isNaN(parsedId)) {
-    res.status(400).send({ msg: "Invalid ID" });
+  if(userIndex === -1){
+    res.status(404).send("User Not Found");
   }
 
-  const userIndex = userData.findIndex((user) => user.id === parsedId);
-
-  if (userIndex === -1) {
-    res.status(404).send({ msg: "User not found" });
+  userData[userIndex] = {
+    id: parsedId,
+    ...body
   }
 
-  userData[userIndex] = { id: parsedId, ...body };
-
-  res.status(200).send(userData[userIndex]);
+  res.status(201).send({
+    message: "User Updated",
+    data: userData[userIndex]
+  });
 });
 
-// * PATCH REQUESTS ( update only specific fields)
+// *4. PATCH Request ( UPDATE SPECIFIC FIELD)
 app.patch("/api/v1/users/:id", (req, res) => {
-  const {
-    body,
-    params: { id },
-  } = req;
-
-  const parsedId = parseInt(id);
-
-  if (isNaN(parsedId)) {
-    res.status(400).send({ msg: "Invalid ID" });
-  }
-
-  const userIndex = userData.findIndex((user) => user.id === parsedId);
-
-  if (userIndex === -1) {
-    res.status(404).send({ msg: "User not found" });
-  }
-
-  userData[userIndex] = { ...userData[userIndex], ...body };
-
-  res.status(200).send(userData[userIndex]);
-});
-// * DELETE REQUESTS
-
-app.delete("/api/v1/users/:id", (req, res) => {
     const {
-        params: { id },
+      body,
+      params: { id },
+  
+      
     } = req;
-    
+  
     const parsedId = parseInt(id);
-    
-    if (isNaN(parsedId)) {
-        res.status(400).send({ msg: "Invalid ID" });
+    const userIndex = userData.findIndex((user)=>user.id === parsedId);
+  
+    if(userIndex === -1){
+      res.status(404).send("User Not Found");
     }
-    
-    const userIndex = userData.findIndex((user) => user.id === parsedId);
-    
-    if (userIndex === -1) {
-        res.status(404).send({ msg: "User not found" });
+  
+    userData[userIndex] = {
+      ...userData[userIndex], ...body
     }
-    
-    userData.splice(userIndex, 1);
-    
-    res.status(204).send(userData);
+  
+    res.status(201).send({
+      message: "User Updated",
+      data: userData[userIndex]
+    });
+  });
+
+// *5. DELETE Request ( it is for deleting data on server)
+
+// Asignment: Implement DELETE Request
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
-app.listen(PORT, () => {
-  console.log("Server is running on port ", PORT);
-});
 
-// loclahost:8000
+// Assignment: Implement DELETE Request
+// filter , find and spread operator , middleware
