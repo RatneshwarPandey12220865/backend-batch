b70e71406ffce6b5403e81ed85f777292e4e62e6
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-12-30 10:26:45 +0530

    added

--- node-js/day-5/index.js
+++ node-js/day-5/index.js
@@ -1,32 +1,25 @@
-const crypto = require('crypto');
+const crypto = require("crypto")
+
+// 1. randomBytes
+const randomvalues = crypto.randomBytes(100);
+
+console.log(randomvalues.toString("hex"))
 
 
-const randomValues = crypto.randomBytes(8);
+// 2. createHash
 
-console.log(randomValues.toString('hex'));
+// const hashvalue = crypto.createHash("sha256").update("Suraj").digest("hex")
 
-
-const hashvalue = crypto.createHash("sha256").update("password").digest("hex");
-// How to convert the hash value to a string
-console.log(hashvalue.toString('hex'));
-console.log(hashvalue)
+// const inputValue = "Surajs"
+// const matchValue = crypto.createHash("sha256").update(inputValue).digest("hex")
 
 
+// if(hashvalue === matchValue){
+//     console.log("you can login")
+// }
+// else{
+//     console.log("Something went wrong")
+// }
 
-
-// crypto modules - Node-js
-
-//* 1. crypto.randomBytes(size, [callback])
-// Purpose: To generate cryptographically strong pseudo-random data.
-// Usage: Useful for creating random tokens, password salts, etc.
-
-// Real-life examples
-
-    // 1. token generation for password reset
-    // 2. api key generation
-    // 3. password salt generation
-    // 4. random string generation
-
-//* 2.  crypot.createHash(algorithm)
-
+// *encryption and decryption
 
